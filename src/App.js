49f7263f5ce79c2a247562dbd19309c65b1872ee
Arun Muraleedharan <arunmuraliam.707@gmail.com>
2021-10-22 17:38:08 +0530

    to do app added

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+
+    const [toDos,setToDos] = useState([]);
+    const [toDo,setToDo] = useState('');
+    
+    function deleteHandler(id) {
+      let filteredList=toDos.filter(itm=>itm.id!==id)
+      setToDos(filteredList)
+    }
+
+  return (
+    <div className="app">
+    <div className="mainHeading">
+      <h1>ToDo List</h1>
+    </div>
+    <div className="subHeading">
+      <br />
+      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+    </div>
+    <div className="input">
+      <input value={toDo} onChange={(e)=>setToDo(e.target.value)}  type="text" placeholder="🖊️ Add item..." />
+      <i onClick={()=>setToDos([...toDos,{id:Date.now(), text:toDo, status:false}])} className="fas fa-plus"></i>
+    </div>
+    <div className="todos">
+     { 
+      toDos.map((obj)=>{
+        return (
+            <div className="todo">
+              <div className="left">
+                <input onChange={(e)=>{
+                  console.log(e.target.checked);
+                  console.log(obj);
+                  setToDos(toDos.filter(obj2=>{
+                    if(obj2.id===obj.id){
+                      obj2.status=e.target.checked
+                    }
+                    return obj2;
+                  }))
+                }} value={obj.status} type="checkbox" name="" id="" />
+
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times" onClick={()=> deleteHandler(obj.id)}></i>
+             </div>
+            </div> 
+          )
+      })}
+
+      {
+        toDos.map((obj)=>{
+          
+          if(obj.status)
+          {
+            return(<h2 align="center">{obj.text}</h2>)
+          }
+        
+          return null
+        })
+      }
+
+    </div>
+  </div>
+  );
+}
+
+export default App;
